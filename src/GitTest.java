6d186dda5367d428b1324b1091ad6c3a1ed5ba38
Karteikay Dhuper <karteikaydhuper@pal-nat186-175-201.itap.purdue.edu>
2022-11-14 14:12:01 -0500

    reset

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,16 +1,5 @@
 public class GitTest {
     public static void main(String[] args){
-        //Karty
-        System.out.println("Karty:");
-        System.out.println("    Cloned file from repo - Karty");
-        System.out.println("    Pushing change to repo - Karty");
-        // Jack
-        System.out.println("Jack:");
-        System.out.println("    Cloned file from repo - Jack");
-        System.out.println("    Pushing change to repo - Jack");
-        // Ryan
-        System.out.println("Ryan:");
-        System.out.println("    Cloned file from repo - Ryan");
-        System.out.println("    Pushing change to repo - Ryan");
+        System.out.println("Everything is gone haha");
     }
 }
