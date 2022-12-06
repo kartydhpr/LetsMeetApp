482d58bb39794a962450a91eae5ecee29b4d6a49
Karteikay Dhuper <karteikaydhuper@Kartys-Laptop.local>
2022-12-06 18:16:41 -0500

    implement maps and zoom functionality

--- src/GUI.java
+++ src/GUI.java
@@ -14,79 +14,47 @@ public class GUI extends JFrame implements ActionListener, KeyListener, MouseLis
     API api = new API();
     
     JFrame frame;
-    JPanel calendarMessagePanel, mapPanel, zoomPanel;
+    JPanel calendarPanel, chatroomPanel, mapPanel, zoomPanel;
     JTabbedPane tabbedPane;
-    JButton getDirectionButton, zoomLinkButton, findRoute;
-    JTextField getDirectionTextBox, zoomLinkTextBox, startDestinationTextBox, endDestinationTextBox;
-    JLabel destinationStartLabel, destinationEndLabel, tempZoolPanelLabel;
-    
+//    JButton ;
+//    JTextField ;
+//    JLabel ;
 
     public GUI(){
+        //General page specsa
         frame = new JFrame();   
-        calendarMessagePanel = new JPanel();    
-        mapPanel = new JPanel();
-        tabbedPane = new JTabbedPane();  
+        tabbedPane = new JTabbedPane();
 
-        calendarMessagePanel = new JPanel();
-        getDirectionTextBox = new JTextField(15);
-        getDirectionTextBox.addActionListener(this);
-        getDirectionButton = new JButton("Get Directions");
-        getDirectionButton.addActionListener(this);
-        zoomLinkTextBox = new JTextField(15);
-        zoomLinkTextBox.addActionListener(this);
-        zoomLinkButton = new JButton("Get Zoom Link");
-        zoomLinkButton.addActionListener(this);
-        calendarMessagePanel.add(getDirectionTextBox);
-        calendarMessagePanel.add(getDirectionButton);
-        calendarMessagePanel.add(zoomLinkTextBox);
-        calendarMessagePanel.add(zoomLinkButton);
-        
+        //Calendar panel specs
+        calendarPanel = new JPanel();
+
+        // Chatroom panel specs
+        chatroomPanel = new JPanel();
+
+        // Map panel specs
         mapPanel = new JPanel();
-        destinationStartLabel = new JLabel("Start Point");
-        startDestinationTextBox = new JTextField(15);
-        startDestinationTextBox.addActionListener(this);
-        destinationEndLabel = new JLabel("End Point");
-        endDestinationTextBox = new JTextField(15);
-        endDestinationTextBox.addActionListener(this);
-        findRoute = new JButton("Find Route");
-        findRoute.addActionListener(this);
-        mapPanel.add(destinationStartLabel);
-        mapPanel.add(startDestinationTextBox);
-        mapPanel.add(destinationEndLabel);
-        mapPanel.add(endDestinationTextBox);
-        mapPanel.add(findRoute);
-        mapPanel.add(api.startApi(System.getProperty("user.dir") + "/src/map.html"), BorderLayout.CENTER);
-        
+        mapPanel.add(api.startApi("maps.google.com"), BorderLayout.CENTER);
+
+        // Zoom panel specs
         zoomPanel = new JPanel();
-        tempZoolPanelLabel = new JLabel("Please enter the Zoom link in the calendar tab.");
-        zoomPanel.add(tempZoolPanelLabel);
-   
-        tabbedPane.add("Calendar", calendarMessagePanel);  
+        zoomPanel.add(api.startApi("zoom.com/join"), BorderLayout.CENTER);
+
+        //tabbed pane at the top of the screen
+        tabbedPane.add("Calendar", calendarPanel);
+        tabbedPane.add("Chat", chatroomPanel);
         tabbedPane.add("Map", mapPanel);
         tabbedPane.add("Zoom", zoomPanel);
 
         frame.add(tabbedPane);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setSize(900, 775);
+        frame.setSize(900, 700);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.setResizable(false);
     }
     
     public void actionPerformed(ActionEvent evt){
-        if(evt.getSource() == getDirectionButton){
-            endDestinationTextBox.setText(getDirectionTextBox.getText());
-            tabbedPane.setSelectedIndex(1);
-        }
-        
-        if(evt.getSource() == zoomLinkButton){
-            zoomPanel.add(api.startApi(zoomLinkTextBox.getText()), BorderLayout.CENTER);
-            tempZoolPanelLabel.setText("");
-            tabbedPane.setSelectedIndex(2);
-        }
-        
-        if(evt.getSource() == findRoute){
-            
-        }
+
     }
     
     //Key listeners
